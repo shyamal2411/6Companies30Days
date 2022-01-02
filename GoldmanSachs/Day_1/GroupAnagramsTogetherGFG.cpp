fd894eea2f6d317df178666afae63f92e224d402
Shyamal <sgprajapati24@gmail.com>
2022-01-02 17:22:33 +0530

    First Day

---
+++ GoldmanSachs/Day_1/GroupAnagramsTogetherGFG.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    vector<vector<string>> Anagrams(vector<string> &string_list)
+    {
+        map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+
+        for (auto str : string_list)
+        {
+            string s = str;
+            sort(s.begin(), s.end());
+            mp[s].push_back(str);
+        }
+        for (auto x : mp)
+            ans.push_back(x.second);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin >> string_list[i];
+        Solution ob;
+        vector<vector<string>> result = ob.Anagrams(string_list);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for (int j = 0; j < result[i].size(); j++)
+            {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
+// } Driver Code Ends
